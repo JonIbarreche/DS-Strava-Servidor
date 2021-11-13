0fef10b01c75cdd6d37dd308ab827df3cbaf1728
Bidi <bidatz.beroiz@opendeusto.es>
2021-11-13 12:21:41 +0100

    commit bidatz

--- src/es/deusto/ingenieria/sd/auctions/server/data/dto/CategoryAssembler.java
+++ src/es/deusto/ingenieria/sd/auctions/server/data/dto/CategoryAssembler.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import es.deusto.ingenieria.sd.auctions.server.data.domain.Category;
 
+
 //This class is part of the DTO pattern. It also implements Singleton Pattern.
 public class CategoryAssembler {
 	private static CategoryAssembler instance;
