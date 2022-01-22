f1977cc89676c503ce46fdc20e3e97f04e0a3a7a
Mikel del Hoyo <mikel.delhoyo@opendeusto.es>
2022-01-22 18:57:23 +0100

    bd

--- bd/create-productsdb.sql
+++ bd/create-productsdb.sql
@@ -6,6 +6,6 @@ DROP USER 'strava_user'@'%';
 /* CREATE ''productsDB' DATABASE */
 CREATE SCHEMA strava;
 /* CREATE THE USER 'products_user' AT LOCAL SERVER WITH PASSWORD 'password' */
-CREATE USER 'strava_user'@'%' IDENTIFIED BY 'localhost';
+CREATE USER 'strava_user'@'%' IDENTIFIED BY 'password';
 /* GRANT FULL ACCESS TO THE DATABASE FOR THE USER 'products_user' AT LOCAL SERVER*/
 GRANT ALL ON strava.* TO 'strava_user'@'%';
